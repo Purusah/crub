fe6b517b494ccca2bf2e92bf45d0b66148945fa2
purusah <bohaichuk.v@gmail.com>
2022-11-27 23:24:03 +0200

    Execution loop

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-1. Keep file open
 1. Write Presto adapter
 1. Execute file with Presto adapter
 1. Run SQL file
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
-#![allow(dead_code)]
+#![warn(clippy::all, clippy::pedantic, clippy::restriction)]
+use std::io::{self, stdout, Write};
 use std::{env, fs};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
 
-struct Output {}
+struct Terminal {
+    _stdout: RawTerminal<std::io::Stdout>,
+}
 
-impl Output {
+impl Terminal {
     pub fn write_raw(&self, data: &String) {
         println!("{}", data)
     }
 
     pub fn init() -> Self {
-        return Output {};
+        return Terminal {
+            _stdout: stdout().into_raw_mode().unwrap(),
+        };
+    }
+
+    pub fn _flush(&self) -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
     }
 }
 
 struct Document {
     pub content: String,
-    pub name: String,
+    pub _name: String,
 }
 
 impl Document {
@@ -23,20 +43,31 @@ impl Document {
         let result = fs::read_to_string(file_name);
         let content = result.unwrap();
         return Self {
-            name: file_name.clone(),
+            _name: file_name.clone(),
             content: content,
         };
     }
 }
 
 struct Application {
-    output: Output,
+    terminal: Terminal,
 }
 
 impl Application {
     pub fn init() -> Self {
         Self {
-            output: Output::init(),
+            terminal: Terminal::init(),
+        }
+    }
+
+    pub fn run(&self, document: &Document) {
+        loop {
+            let key = Terminal::read_key().unwrap();
+            match key {
+                Key::Char('\n') => self.terminal.write_raw(&document.content),
+                Key::Esc => break,
+                _ => (),
+            }
         }
     }
 }
@@ -48,7 +79,8 @@ fn main() {
     if maybe_file_name.is_none() {
         return;
     }
+
     let file_name = maybe_file_name.unwrap();
     let document = Document::init(file_name);
-    app.output.write_raw(&document.content);
+    app.run(&document);
 }
