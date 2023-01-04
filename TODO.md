839ac7a9ab310b8117afb2ecb54b0ce7a2e0b8af
purusah <bohaichuk.v@gmail.com>
2023-01-04 20:13:39 +0200

    Execute request on button click

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 1. Write Presto adapter
 1. Execute file with Presto adapter
 1. Run SQL file
+1. with Dependency Injection and pass request into
--- src/client/app.rs
+++ src/client/app.rs
@@ -1,13 +1,16 @@
 use gloo_net::http::Request;
 use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
+use yew::{html, Callback};
 
 #[function_component]
 pub fn App() -> Html {
     let response = use_state_eq(|| String::from(""));
-    {
+
+    let onclick = {
         let response = response.clone();
-        use_effect(|| {
+        Callback::from(move |_: MouseEvent| {
+            let response = response.clone();
             spawn_local(async move {
                 let result = Request::get("https://httpbin.org/ip")
                     .send()
@@ -18,24 +21,23 @@ pub fn App() -> Html {
                     .unwrap();
                 response.set(result);
             });
-            ()
-        });
-    }
+        })
+    };
 
     html! {
         <>
             <div>
-                <p>
-                    {(*response).clone()}
-                </p>
-            </div>
-            <div id="text-input" contenteditable="true"></div>
-            <div>
-                <button id="run-query" class="run-button" type="button">
+                <button id="run-query" class="run-button" type="button" onclick={onclick}>
                     <p>
                         { "Run" }
                     </p>
                 </button>
+            </div>
+            <div class="editor">
+                <div id="editor-query" contenteditable="true"></div>
+                <div id="editor-output">
+                    {(*response).clone()}
+                </div>
             </div>
         </>
     }
